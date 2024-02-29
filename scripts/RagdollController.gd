92eea506233151906651031ea34b78b6448b21c6
KirbyAndStuff <nguungoku@gmail.com>
2024-02-29 16:52:58 +0100

    wall jump limit

--- scripts/RagdollController.gd
+++ scripts/RagdollController.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-var power = 500
-const JUMP_VELOCITY = 20
+var power = 300
+const JUMP_VELOCITY = 33
 var can_jump = false
 var can_jumpnumber = 0
+var can_jumplimit = 0
+
 # Limb Controls
 var limbs = []
 
@@ -17,9 +19,11 @@ func _physics_process(delta):
 	$Limbs/Torso.apply_impulse(Vector2.RIGHT * axis * power, Vector2.ZERO)
 	$Limbs/Torso.apply_impulse(Vector2.UP * 5 * axis_y * power, Vector2.ZERO)
 	# Jumping
-	if Input.is_action_just_pressed("ui_up") and can_jump:
+	if Input.is_action_just_pressed("ui_up") and can_jump and can_jumplimit < 2:
 		$Limbs/Torso.apply_impulse(Vector2.UP * JUMP_VELOCITY * power, Vector2.ZERO)
-		
+		can_jumplimit += 1
+		print(can_jumplimit)
+
 	# Loop Over All Limbs
 	for limb in $Limbs.get_children():
 		if limb is Limb && limb.enabled == true:
@@ -49,3 +53,5 @@ func _on_area_2d_body_exited(body):
 		if can_jumpnumber == 0:
 			can_jump = false
 			print('wall jump off')
+			if can_jumplimit == 2:
+				can_jumplimit -= 2
